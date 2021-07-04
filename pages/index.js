769f8c0d9b7f83f68ac8fa0f12172b1586f41113
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-04 13:43:39 -0400

    new

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,30 @@ export default function Home() {
             Buy Now
           </button>
         </nav>
+        <div className="bg-grey-50 p-2">
+          <div className="albert	 flex flex-col px-8 py-8 rounded-lg">
+            <h1 className="text-white font-bold text-2xl text-center">
+              Share all your socials with just a tap
+            </h1>
+            <video
+              className="hero__video h-48"
+              preload=""
+              muted=""
+              playsinline=""
+              autoplay=""
+              loop=""
+            >
+              <source
+                src="https://cdn.shopify.com/s/files/1/0572/4058/4379/t/1/assets/tap.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div className="text-white text-center">Stuff goes here</div>
+            <button className="bg-purple-400 px-4 py-1 text-white rounded-md text-lg">
+              Buy Now
+            </button>
+          </div>
+        </div>
       </main>
     </>
   );
