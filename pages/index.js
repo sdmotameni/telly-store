53c917131c3a9a5bc353213eef0056352f13cff4
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-04 14:56:00 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -12,33 +12,35 @@ export default function Home() {
 
       <main>
         <nav className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 flex items-center	justify-between px-3 py-3">
-          <Image src="/nexar.png" alt="" width={65} height={25} />
-          <button className="bg-purple-400 px-4 py-1 text-white rounded-md text-sm">
+          <button className="bg-blue-600 px-4 py-1 text-white rounded-md text-sm">
+            Login
+          </button>
+          <Image src="/nexar.png" alt="" width={68} height={25} />
+          <button className="bg-purple-500 px-4 py-1 text-white rounded-md text-sm">
             Buy Now
           </button>
         </nav>
-        <div className="bg-grey-50 p-2">
-          <div className="albert	 flex flex-col px-8 py-8 rounded-lg">
-            <h1 className="text-white font-bold text-2xl text-center">
+        <div className="bg-grey-100 p-2">
+          <div className="custombg flex flex-col px-8 py-8 rounded-lg">
+            <h1 className="text-white font-bold text-3xl text-center">
               Share all your socials with just a tap
             </h1>
-            <video
-              className="hero__video h-48"
-              preload=""
-              muted=""
-              playsinline=""
-              autoplay=""
-              loop=""
-            >
+            <video autoplay="" playsinline="" loop="" muted="" className="h-48">
               <source
-                src="https://cdn.shopify.com/s/files/1/0572/4058/4379/t/1/assets/tap.mp4"
-                type="video/mp4"
+                src="https://cdn.shopify.com/s/files/1/0572/4058/4379/t/1/assets/tap.webm"
+                type="video/webm"
               />
+              <source src="https://cdn.shopify.com/s/files/1/0572/4058/4379/t/1/assets/tap.mp4" />
+              <source src="https://cdn.shopify.com/s/files/1/0572/4058/4379/t/1/assets/tap.mp4" />
             </video>
             <div className="text-white text-center">Stuff goes here</div>
-            <button className="bg-purple-400 px-4 py-1 text-white rounded-md text-lg">
+            <button className="bg-purple-500 px-4 py-1 text-white rounded-md text-lg">
               Buy Now
             </button>
+            <div className="text-white text-sm">
+              Free shipping & returns | 15-day money-back guarantee | No monthly
+              fees
+            </div>
           </div>
         </div>
       </main>
