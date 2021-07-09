4ddad811cd0bb0b09a9c05a7f35627b898bb209a
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-08 20:30:32 -0400

    push

--- pages/common/navbar.js
+++ pages/common/navbar.js
@@ -4,8 +4,17 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 flex items-center justify-between px-5 py-4 bg-purple-500">
       <div className="flex items-center">
-        <img src="/navicon.svg" className="w-4 mr-2" />
-        <img src="/logo.svg" className="w-12" />
+        <div className="relative w-4 h-4 mr-2">
+          <Image
+            alt="navicon"
+            src="/navicon.svg"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-12 h-5">
+          <Image alt="logo" src="/logo.svg" layout="fill" objectFit="contain" />
+        </div>
       </div>
       <a
         href="https://gettelly.com/products/telly-gadget"
