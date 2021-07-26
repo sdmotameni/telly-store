f513a143dcf7fc44f5c2b22b73e644ac947aaf43
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-26 19:26:14 -0400

    Smaller video height

--- pages/hero.js
+++ pages/hero.js
@@ -24,7 +24,7 @@ export default function Hero() {
             <h1 className="mt-3 text-3xl font-bold text-center">
               Share all your socials with a tap
             </h1>
-            <video className="w-auto h-96" autoPlay loop muted>
+            <video className="w-auto h-80" autoPlay loop muted>
               <source src="/tap.webm" type="video/webm" />
               <source src="/tap.mp4" type="video/mp4" />
               Your browser does not support the video tag.
