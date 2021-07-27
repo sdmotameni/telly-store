7dd462eb889ab25992b91938d94c7a9df252a6a1
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-27 17:08:46 -0400

    more changes

--- pages/components/HeroTapVideo.js
+++ pages/components/HeroTapVideo.js
@@ -1,21 +1,5 @@
-import React, { useState, useCallback } from "react";
-import ReactPlayer from "react-player";
-
 export default function HeroTapVideo() {
   return (
-    // <video className="w-full h-80" autoPlay loop muted>
-    //   <source src={TapVideo} type="video/mp4" />
-    //   Your browser does not support the video tag.
-    // </video>
-    // <ReactPlayer
-    //   url="https://www.video-url.com"
-    //   playing={isPlaying}
-    //   controls
-    //   onContextMenu={handleContextMenu}
-    // />
-    // <video className="w-full h-80" autoPlay loop muted preload={auto}>
-    //   <source src="/tap-bluebg.mp4" type="video/mp4" />
-    // </video>
     <video className="w-full h-80" autoPlay playsInline loop muted>
       <source
         src="https://us.getnexar.com/_next/static/videos/day-f744cb887147aa49ab08d08031aafffe.mp4"
