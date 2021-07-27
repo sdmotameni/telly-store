393f01dfa58b4bbe58f852e6d235018c2c180c19
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-27 17:07:18 -0400

    fixed autoplaying before fixing serve problem

--- pages/components/HeroTapVideo.js
+++ pages/components/HeroTapVideo.js
@@ -16,14 +16,14 @@ export default function HeroTapVideo() {
     // <video className="w-full h-80" autoPlay loop muted preload={auto}>
     //   <source src="/tap-bluebg.mp4" type="video/mp4" />
     // </video>
-    <video className="w-full h-80" autoPlay playsinline="" loop muted="">
-      <source
-        src="https://us.getnexar.com/_next/static/videos/day-sm-26e976708354751d886f49859ae35e02.webm"
-        type="video/webm"
-      />
+    <video className="w-full h-80" autoPlay playsInline loop muted>
       <source
         src="https://us.getnexar.com/_next/static/videos/day-f744cb887147aa49ab08d08031aafffe.mp4"
         type="video/mp4"
+      />
+      <source
+        src="https://us.getnexar.com/_next/static/videos/day-sm-26e976708354751d886f49859ae35e02.webm"
+        type="video/webm"
       />
     </video>
   );
