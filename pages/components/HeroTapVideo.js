7f31258d9aa3370f145360bf80146a371d8447c7
Sep (Macbook Pro) <me@sepmotameni.com>
2021-07-27 17:45:33 -0400

    Fixed video formats

--- pages/components/HeroTapVideo.js
+++ pages/components/HeroTapVideo.js
@@ -2,12 +2,12 @@ export default function HeroTapVideo() {
   return (
     <video className="w-full h-80" autoPlay playsInline loop muted>
       <source
-        src="https://us.getnexar.com/_next/static/videos/day-f744cb887147aa49ab08d08031aafffe.mp4"
-        type="video/mp4"
+        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1627422241/videos/tap-bluebg-o_cs4nm5.webm"
+        type="video/webm"
       />
       <source
-        src="https://us.getnexar.com/_next/static/videos/day-sm-26e976708354751d886f49859ae35e02.webm"
-        type="video/webm"
+        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1627422189/videos/tap-bluebg_esg2pb.mp4"
+        type="video/mp4"
       />
     </video>
   );
