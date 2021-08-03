46b94609a7a1ad347e7b6d1abd5c60d29eeb08ce
Sep (Macbook Pro) <me@sepmotameni.com>
2021-08-03 16:32:07 -0400

    New hero

--- components/HeroTapVideo.js
+++ components/HeroTapVideo.js
@@ -1,10 +1,10 @@
 export default function HeroTapVideo() {
   return (
     <video className="w-full h-80" autoPlay playsInline loop muted>
-      {/* <source
-        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1627422241/videos/tap-bluebg-o_cs4nm5.webm"
+      <source
+        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1628022652/videos/telly_bluebg-o_yybrmy.webm"
         type="video/webm"
-      /> */}
+      />
       <source
         src="https://res.cloudinary.com/dyusynvjw/video/upload/v1628020871/videos/telly_bluebg-o_iqu1t5.mp4"
         type="video/mp4"
