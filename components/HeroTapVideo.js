5af2a71e6de9ff2a6c2efdb81b59dec1f7b65ad8
Sep (Macbook Pro) <me@sepmotameni.com>
2021-08-03 16:36:27 -0400

    Updated HeroTapVideo urls

--- components/HeroTapVideo.js
+++ components/HeroTapVideo.js
@@ -2,11 +2,11 @@ export default function HeroTapVideo() {
   return (
     <video className="w-full h-80" autoPlay playsInline loop muted>
       <source
-        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1628022652/videos/telly_bluebg-o_yybrmy.webm"
+        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1628022909/videos/telly_bluebg-o_vc5k26.webm"
         type="video/webm"
       />
       <source
-        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1628020871/videos/telly_bluebg-o_iqu1t5.mp4"
+        src="https://res.cloudinary.com/dyusynvjw/video/upload/v1628022908/videos/telly_bluebg-o_oxvxqi.mp4"
         type="video/mp4"
       />
     </video>
