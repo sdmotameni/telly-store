501d6ff0f6183b1d68084ada0babf9591ae1597f
Sep Motameni <sepmotameni@Seps-MBP.attlocal.net>
2021-12-30 16:05:12 -0500

    Added /help and /privacy

---
+++ pages/help.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Help() {
+  return (
+    <div>
+      We love to talk! Shoot us over an email anytime and we will respond to you
+      within 24 hours. Email: support@gettelly.com
+    </div>
+  );
+}
